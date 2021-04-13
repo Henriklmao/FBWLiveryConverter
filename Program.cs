98ebb680522f06545cdf9ab248b19efbc82278ff
wunderkekshd <wunderkekshd@protonmail.com>
2021-04-13 17:55:09 +0200

    1

--- Program.cs
+++ Program.cs
@@ -30,7 +30,7 @@ namespace LiveryConverter
     {
         public static void Conversion()
         {
-            if (File.Exists(lvrdir + "/manifest.json"))
+            if (File.Exists(lvrdir + "manifest.json"))
             {
 
             }
@@ -45,31 +45,31 @@ namespace LiveryConverter
                     /// Backup aircraft.cfg
                     System.IO.File.Move("/SimObjects/Airplanes/*/aircraft.cfg", "/SimObjects/Airplanes/*/.aircraft.cfg");
                     /// Edit aircraft.cfg
-                    string text0 = File.ReadAllText("/SimObjects/Airplanes/*/.aircraft.cfg");
+                    string text0 = File.ReadAllText("SimObjects/Airplanes/*/.aircraft.cfg");
                     string replace0 = text0.Replace("base_container = \"..\\Asobo_A320_NEO\"", "base_container = \"..\\FlyByWire_A320_NEO\"");
-                    File.WriteAllText("/SimObjects/Airplanes/*/aircraft.cfg", replace0);
+                    File.WriteAllText("SimObjects/Airplanes/*/aircraft.cfg", replace0);
 
-                    string text1 = File.ReadAllText("/SimObjects/Airplanes/*/aircraft.cfg");
+                    string text1 = File.ReadAllText("SimObjects/Airplanes/*/aircraft.cfg");
                     string replace1 = text1.Replace("base_container = \"../Asobo_A320_NEO\"", "base_container = \"..\\FlyByWire_A320_NEO\"");
-                    File.WriteAllText("/SimObjects/Airplanes/*/aircraft.cfg", replace1);
+                    File.WriteAllText("SimObjects/Airplanes/*/aircraft.cfg", replace1);
                 }
                 else
                 {
                     MessageBox.Show("The aircraft.cfg file of the selected livery couldn't be found. Please read the documentations.", "Exception: Livery file missing",
                         MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
-                if (File.Exists(lvrdir + "/SimObjects/Airplanes/*/model.main/model.cfg"))
+                if (File.Exists(lvrdir + "SimObjects/Airplanes/*/model.main/model.cfg"))
                 {
                     /// Backup model.cfg
-                    System.IO.File.Move("/SimObjects/Airplanes/*/model.main/model.cfg", "/SimObjects/Airplanes/*/model.main/model.cfg");
+                    System.IO.File.Move("SimObjects/Airplanes/*/model.main/model.cfg", "/SimObjects/Airplanes/*/model.main/model.cfg");
                     /// Edit model.cfg
-                    string text2 = File.ReadAllText(lvrdir + "/SimObjects/Airplanes/*/model.main/.model.cfg");
+                    string text2 = File.ReadAllText(lvrdir + "SimObjects/Airplanes/*/model.main/.model.cfg");
                     string replace2 = text2.Replace("exterior = .. / .. / Asobo_A320_NEO / model / A320_NEO.xml", "exterior=../../FlyByWire_A320_NEO/model/A320_NEO.xml");
                     File.WriteAllText("output file", replace2);
 
-                    string text3 = File.ReadAllText(lvrdir + "/SimObjects/Airplanes/*/model.main/.model.cfg");
+                    string text3 = File.ReadAllText(lvrdir + "SimObjects/Airplanes/*/model.main/.model.cfg");
                     string replace3 = text2.Replace("exterior = .. \\ .. \\ Asobo_A320_NEO \\ model \\ A320_NEO.xml", "exterior=../../FlyByWire_A320_NEO/model/A320_NEO.xml");
-                    File.WriteAllText(lvrdir + "/SimObjects/Airplanes/*/model.main/model.cfg", replace3);
+                    File.WriteAllText(lvrdir + "SimObjects/Airplanes/*/model.main/model.cfg", replace3);
                 }
                 else
                 {
@@ -85,11 +85,11 @@ namespace LiveryConverter
                     /// Edit model.cfg
                     string text4 = File.ReadAllText("/SimObjects/Airplanes/*/model.main/.model.cfg");
                     string replace4 = text4.Replace("interior = .. / .. / Asobo_A320_NEO / model / A320_NEO_interior.xml", "exterior=../../FlyByWire_A320_NEO/model/A320_NEO_Interior.xml");
-                    File.WriteAllText(lvrdir + "/SimObjects/Airplanes/*/model.main/model.cfg", replace4);
+                    File.WriteAllText(lvrdir + "SimObjects/Airplanes/*/model.main/model.cfg", replace4);
 
                     string text5 = File.ReadAllText("/SimObjects/Airplanes/*/model.main/.model.cfg");
                     string replace5 = text5.Replace("interior = .. \\ .. \\ Asobo_A320_NEO \\ model \\ A320_NEO_interior.xml", "inter=../../FlyByWire_A320_NEO/model/A320_NEO_Interior.xml");
-                    File.WriteAllText(lvrdir + "/SimObjects/Airplanes/*/model.main/model.cfg", replace5);
+                    File.WriteAllText(lvrdir + "SimObjects/Airplanes/*/model.main/model.cfg", replace5);
                 }
                 else
                 {
